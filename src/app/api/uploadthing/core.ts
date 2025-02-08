d542584ec64c5f453191d4d02d8bd1594ec351e9
unav4ila8le <leonardobalduzzi@me.com>
2025-02-08 18:08:54 +0900

    connected userID to db

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -17,7 +17,7 @@ export const ourFileRouter = {
        * @see https://docs.uploadthing.com/file-routes#route-config
        */
       maxFileSize: "4MB",
-      maxFileCount: 1,
+      maxFileCount: 10,
     },
   })
     // Set permissions and file types for this FileRoute
@@ -33,11 +33,10 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
-
       await db.insert(images).values({
         name: file.name,
         url: file.url,
+        userId: metadata.userId,
       });
 
       console.log("file url", file.url);
