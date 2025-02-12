4b097371e29e7a137acf91a95776fb7aee418005
unav4ila8le <leonardobalduzzi@me.com>
2025-02-12 16:12:19 +0900

    parallel routes styled

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,11 +21,10 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="m-0 h-screen max-h-full w-screen max-w-full bg-black/80"
+      className="m-0 h-screen max-h-full w-screen max-w-full bg-black/90 text-white"
       onClose={onDismiss}
     >
       {children}
-      <button onClick={onDismiss} className="close-button" />
     </dialog>,
     document.getElementById("modal-root")!,
   );
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,18 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} alt={image.name} className="w-64" />;
+  return (
+    <div className="flex h-full max-h-full w-full max-w-full">
+      <div className="flex flex-grow items-center justify-center">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="h-full w-full object-contain"
+        />
+      </div>
+      <div className="flex w-80 flex-shrink-0 flex-col justify-center p-4">
+        <h1 className="break-all text-xl font-bold">{image.name}</h1>
+      </div>
+    </div>
+  );
 }
