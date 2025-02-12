e5e98251fb41db51d34d3ba9c95e7a9712971472
unav4ila8le <leonardobalduzzi@me.com>
2025-02-12 16:58:21 +0900

    finalized modal and parallel routes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,17 +2,19 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+
   return (
     <div className="flex h-full max-h-full w-full max-w-full">
-      <div className="flex flex-grow items-center justify-center">
+      <div className="flex flex-grow items-center justify-center border-r border-white">
         <img
           src={image.url}
           alt={image.name}
           className="h-full w-full object-contain"
         />
       </div>
-      <div className="flex w-80 flex-shrink-0 flex-col justify-center p-4">
-        <h1 className="break-all text-xl font-bold">{image.name}</h1>
+      <div className="flex w-80 flex-shrink-0 flex-col gap-2 p-4">
+        <h1 className="break-all text-lg font-bold">{image.name}</h1>
+        <div>Created on: {new Date(image.createdAt).toLocaleDateString()}</div>
       </div>
     </div>
   );
